b42a78cc7f18298c0412b225d5ae5c5431af5b9b
Joel Malinao <joe.malski@gmail.com>
2019-07-18 15:16:05 +0800

    used flask-wtf in login form, change favicon and add school-logo

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,15 @@
 from app import app
 from flask import render_template
+from app.forms import LoginForm
+
+
+@app.route('/login')
+def login():
+	form = LoginForm()
+	return render_template('login.html', title='Sign In', form=form)
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return render_template('index.html')
-
-
+    return render_template('index.html')
--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,8 +1,19 @@
 // main.js - app's main javascript file
 
 $(function () {
-  console.log('hello world! - jquery');
+    console.log('hello world! - jquery');
 
-  $('[data-toggle="popover"]').popover();
+    $('[data-toggle="popover"]').popover();
+
+    var form = document.querySelector('.needs-validation');
+
+    form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+
+        form.classList.add('was-validated')
+    });
 
 });
