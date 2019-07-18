d9448f3514b9e8d161e0d9e48b2f46a28978c0fe
Joel Malinao <joe.malski@gmail.com>
2019-07-18 16:18:40 +0800

    add login post and redirection

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,14 @@
 from app import app
-from flask import render_template
+from flask import render_template, redirect ,url_for
 from app.forms import LoginForm
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-	form = LoginForm()
-	return render_template('login.html', title='Sign In', form=form)
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
 
 
 @app.route('/')
