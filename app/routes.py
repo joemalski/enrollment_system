53491299d780a27fe634aecbd0ec54ffe129393f
Joel Malinao <joe.malski@gmail.com>
2019-07-25 03:32:38 +0800

    login(), remove the 'title' argument in render_template()

--- app/routes.py
+++ app/routes.py
@@ -10,7 +10,7 @@ def login():
     if form.validate_on_submit():
         return redirect(url_for('index'))
 
-    return render_template('login.html', title='Sign In', form=form)
+    return render_template('login.html', form=form)
 
 
 @app.route('/')
