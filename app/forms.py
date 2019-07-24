9af2e3d5cacb49d06cdcc59562d5d24f0b044788
Joel Malinao <joe.malski@gmail.com>
2019-07-25 03:06:37 +0800

    implement a full server side validation in login form

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,22 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length
 
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    username = StringField('Username', validators=[
+        DataRequired(message='Username is required!'),
+        Length(
+            min=5,
+            max=8, 
+            message='Username must be bewteen 5-8 characters long!'
+        )
+    ])
+    password = PasswordField('Password', validators=[
+        DataRequired(message='Password is required!'),
+        Length(
+            min=6,
+            message='Password must be atleast 6 characters!'
+        )
+    ])
     submit = SubmitField('Sign In')
--- app/static/js/main.js
+++ app/static/js/main.js
@@ -5,16 +5,22 @@ $(function () {
 
     $('[data-toggle="popover"]').popover();
 
+    /*
+    Note: If you wanted to implement client-side validation can enable
+          this code block and add 'needs-validation' class to the form.
+
     var form = document.querySelector('.needs-validation');
 
     if(form) {
         form.addEventListener('submit', function(event) {
+            
             if (form.checkValidity() === false) {
                 event.preventDefault();
                 event.stopPropagation();
             }
-
+            
             form.classList.add('was-validated')
         });        
     }
+    */
 });
