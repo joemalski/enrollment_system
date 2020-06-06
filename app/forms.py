d96fad28dc3841fdafe61c11bc1b4a440dcbc83e
Joel Malinao <joe.malski@gmail.com>
2020-06-06 14:22:41 +0800

    fix indent

--- app/forms.py
+++ app/forms.py
@@ -10,17 +10,13 @@ class LoginForm(FlaskForm):
         Length(
             min=5,
             max=8, 
-            message='Username must be bewteen 5-8 characters long!'
-        )
-    ])
+            message='Username must be bewteen 5-8 characters long!')])
 
     password = PasswordField('Password', validators=[
         DataRequired(message='Password is required!'),
         Length(
             min=6,
-            message='Password must be atleast 6 characters!'
-        )
-    ])
+            message='Password must be atleast 6 characters!')])
 
     remember_me = BooleanField('Remember Me')
 
