8bc9fd72b1e1bc73bc1314973c96c95d82b4f1d0
Joel Malinao <joe.malski@gmail.com>
2020-06-06 14:23:50 +0800

    fix spaces

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from app import db, login
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
