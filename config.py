6a345c2677b992ca77736a3a5c2e637f478d7915
Joel Malinao <joe.malski@gmail.com>
2020-06-06 14:06:36 +0800

    fix config file

--- config.py
+++ config.py
@@ -1,9 +1,8 @@
 import os
-basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'this-is-a-secret'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'mysql+pymysql://root:123456jj@localhost/enrollment'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
